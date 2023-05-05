b05217b1c5b09c2394d22e434a3c3337f882b5a7
adamh0807 <adamhavakook@gmail.com>
2023-05-05 09:55:01 +0300

    the last version

---
+++ try.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+
+echo "# lesson-30" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/adamh0807/lesson-30.git
+git push -u origin main
+
